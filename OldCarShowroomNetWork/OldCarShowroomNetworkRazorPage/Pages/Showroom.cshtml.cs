2bf3938c886d33c2f328bd32c37cd9ea2e6a68d3
taolaprovip <tranthequanganime@gmail.com>
2023-05-30 23:52:01 +0700

    Update Showroom

---
+++ OldCarShowroomNetWork/OldCarShowroomNetworkRazorPage/Pages/Showroom.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OldCarShowroomNetworkRazorPage.Pages
+{
+    public class ShowroomModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
