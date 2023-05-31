b837d0110172ea97ea559ec9839ded330dfc070c
thangpx189 <104499944+thangpx189@users.noreply.github.com>
2023-05-31 23:17:26 +0700

    Backup.

--- OldCarShowroomNetWork/OldCarShowroomNetworkRazorPage/Program.cs
+++ OldCarShowroomNetWork/OldCarShowroomNetworkRazorPage/Program.cs
@@ -1,41 +1,8 @@
-using BOs.Models;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.Google;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using REPOs;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddDbContext<OldCarShowroomNetworkContext>(
-        options => options.UseSqlServer("name=ConnectionStrings:DB"));
-builder.Services.AddScoped<UserRepository>();
-builder.Services.Configure<CookiePolicyOptions>(options =>
-{
-    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-    options.CheckConsentNeeded = context => true;
-    options.MinimumSameSitePolicy = SameSiteMode.None;
-});
-builder.Services.AddSession(options =>
-{
-    options.IdleTimeout = TimeSpan.FromMinutes(60);
-    options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
-});
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
-})
-            .AddCookie()
-            .AddGoogle(GoogleDefaults.AuthenticationScheme, options =>
-            {
-                options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
-                options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
-            });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -49,12 +16,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.UseSession();
-
 app.MapRazorPages();
 
 app.Run();
-
-app.UseAuthentication();
-app.UseSession();
-app.UseCookiePolicy();
